2090773236825c352e335c211974a6650720b9e2
Miguel Maldonado <m_angel_mm@yahoo.com.mx>
2023-11-25 19:08:52 -0600

    Fixed rubocop offenses

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# movies controller
 class MoviesController < ApplicationController
   def new; end
 
--- db/migrate/20231126010150_create_movies.rb
+++ db/migrate/20231126010150_create_movies.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Create movies table
 class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
